517686ab0e31c439dd556ec24bdccd4c2eb609e6
Nuri Fetiev <nurfet3@gmail.com>
2023-10-06 14:41:56 +0300

    Java pre-project. Практическая задача 2.1.5

---
+++ src/main/java/koschei/models/Deth8.java
@@ -0,0 +1,12 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Deth8 {
+
+    public String toString() {
+        return "";
+    }
+}
---
+++ src/main/java/koschei/models/Needle7.java
@@ -0,0 +1,16 @@
+package koschei.models;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class Needle7 {
+
+    private Deth8 deth8;
+
+
+    @Override
+    public String toString() {
+        return ", смерть Кощея на игле :( " + "";
+    }
+}
